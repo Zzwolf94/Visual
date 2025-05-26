a728af29bbe73fb0b6895eb9ed24cf634bd20fc2
Zzwolf94 <71764205+Zzwolf94@users.noreply.github.com>
2025-05-26 13:58:19 +0200

    suicide

--- JavaScript/Ejercicio4.js
+++ JavaScript/Ejercicio4.js
@@ -1,5 +1,72 @@
- function suma(x, y){
-  let resultado = x + y;
-  alert("La suma de " + x + " y " + y + " es: " + resultado);
-  return resultado;
- }
+const tareas_pendientes = [];
+const tareas_completadas = [];
+const textomenu =
+  "Seleccione una opción:\n1. Añadir tarea\n2. Ver tareas pendientes\n3. Ver tareas completadas\n4. Completar tarea\n5. Salir";
+
+function CrearTarea() {
+  const tarea = prompt("Ingrese la tarea:");
+  tareas_pendientes.push(tarea);
+}
+function VerTareasPendientes() {
+  if (tareas_pendientes.length === 0) {
+    alert("No hay tareas pendientes.");
+  } else {
+    let texto = "";
+    tareas_pendientes.forEach(tarea => texto += tarea + " \n");
+    alert(texto)
+  }
+}
+function VerTareasCompletadas() {
+  if (tareas_pendientes.length === 0) {
+    alert("No hay tareas pendientes.");
+  } else {
+    let texto = "";
+    tareas_completadas.forEach(tarea => texto += tarea + " \n");
+    alert(texto)
+  }
+}
+function CompletarTarea(){
+    if (tareas_pendientes.length === 0) {
+    alert("No hay tareas pendientes.");
+  } else {
+    let completadas = "";
+    tareas_pendientes.forEach((tarea, index) => completadas += index + " " + tarea + " \n");
+    alert(completadas)
+  }
+
+}
+function main() {
+  let salir = true;
+  while (salir === true) {
+    const userinput = prompt(textomenu);
+    switch (userinput) {
+      case "1":
+        // Añadir tarea
+        CrearTarea();
+        alert("Tarea añadida correctamente");
+        break;
+      case "2":
+        // Ver tareas pendientes
+        VerTareasPendientes();
+        break;
+      case "3":
+        // Ver tareas completadas
+        VerTareasCompletadas();
+        break;
+      case "4":
+        // Completar tarea
+        CompletarTarea();
+        break;
+      case "5":
+        // Salir
+        alert("Saliendo del programa");
+        salir = false;
+        break;
+      default:
+        alert("Opción no válida");
+        break;
+    }
+  }
+}
+
+main()
